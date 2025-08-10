9e566b113ed7911ad9140d3b3742e30f76fd8b01
colin-404 <1712472820@qq.com>
2025-08-10 09:28:55 +0800

    init

---
+++ db/mongo.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/colin-404/logx"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	MongoClient *mongo.Client
+	MongoDB     *mongo.Database
+)
+
+// InitMongoDB 初始化MongoDB连接
+func InitMongoDB(dbName string, mongoURI string) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		logx.Errorf("Failed to connect to MongoDB: %v", err)
+		return err
+	}
+
+	// test connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		logx.Errorf("Failed to ping MongoDB: %v", err)
+		return err
+	}
+
+	logx.Infof("Successfully connected to MongoDB")
+
+	MongoClient = client
+	MongoDB = client.Database(dbName)
+
+	return nil
+}
+
+// CloseMongoDB
+func CloseMongoDB() error {
+	if MongoClient != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		return MongoClient.Disconnect(ctx)
+	}
+	return nil
+}
+
+// GetCollection
+func GetCollection(collectionName string) *mongo.Collection {
+	return MongoDB.Collection(collectionName)
+}
